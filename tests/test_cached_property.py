30671907e2aaeab4655fcd3ebbd4598543121523
Mital Ashok <mital.vaja@googlemail.com>
2017-06-29 20:35:13 +0100

    Added tests, cached_property, singletons and README

---
+++ tests/test_cached_property.py
@@ -0,0 +1,85 @@
+import unittest
+
+from objecttools.cached_property import *
+
+
+class TestCachedProperty(unittest.TestCase):
+    def test_cache(self):
+        get_access = []
+
+        class Test(object):
+            @CachedProperty
+            def x(self):
+                get_access.append(True)
+                return 1
+
+        t = Test()
+        self.assertEqual(t.x, 1, 'Incorrect result from property cache!')
+        self.assertEqual(t.x, 1, 'Incorrect result from property cache!')
+        self.assertEqual(len(get_access), 1, 'Accessed too many times!')
+
+    def test_no_set(self):
+        class Test(object):
+            @CachedProperty
+            def x(self):
+                return 1
+
+        t = Test()
+        with self.assertRaises(AttributeError):
+            t.x = 2
+        self.assertEqual(t.x, 1, 'Incorrectly set read-only property!')
+
+    def test_no_del(self):
+        class Test(object):
+            @CachedProperty
+            def x(self):
+                return 1
+
+        t = Test()
+        with self.assertRaises(AttributeError):
+            del t.x
+
+    def test_with_del(self):
+        get_access = []
+
+        class Test(object):
+            @CachedProperty
+            def x(self):
+                get_access.append(True)
+                return len(get_access)
+
+            @x.deleter
+            def x(self):
+                pass
+
+        t = Test()
+        self.assertEqual(t.x, 1, 'Did not call getter properly')
+        self.assertEqual(t.x, 1, 'Did not cache properly')
+        del t.x
+        self.assertEqual(t.x, 2, 'Did not invalidate cache properly')
+        self.assertEqual(t.x, 2, 'Did not cache after deleting')
+        self.assertEqual(len(get_access), 2, 'Unexpected usage of getter')
+
+    def test_with_set(self):
+        get_access = []
+
+        class Test(object):
+            @CachedProperty
+            def x(self):
+                get_access.append(True)
+                return len(get_access)
+
+            @x.setter
+            def x(self, value):
+                pass
+
+        t = Test()
+        self.assertEqual(t.x, 1, 'Did not call getter properly')
+        self.assertEqual(t.x, 1, 'Did not cache properly')
+        t.x = 12
+        self.assertEqual(t.x, 12, 'Did not set cache properly')
+        self.assertEqual(t.x, 12, 'Did not get cache properly')
+        self.assertEqual(len(get_access), 1, 'Unexpected usage of getter')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_singletons.py
@@ -0,0 +1,37 @@
+import unittest
+
+from objecttools.singletons import *
+
+@Singleton.as_decorator
+class Test(object):
+    pass
+
+
+class TestSingletons(unittest.TestCase):
+    def test_identity(self):
+        a = Test()
+        b = Test()
+        self.assertIs(a, b)
+
+    def test_subclass_identity(self):
+        class TestB(Test):
+            pass
+
+        @Singleton.as_decorator
+        class TestC(Test):
+            pass
+
+        a = Test()
+        b = TestB()
+        c = TestC()
+        self.assertIs(a, b)
+        self.assertIs(a, c)
+        self.assertIs(b, c)
+
+    def test_representation(self):
+        singleton = Singleton.create('Singleton', object_name='singleton')()
+        self.assertEqual(repr(singleton), 'singleton', 'Instance not assigned proper __repr__')
+        self.assertEqual(repr(type(singleton)), 'Singleton', 'Class not assigned proper __repr__')
+
+if __name__ == '__main__':
+    unittest.main()
